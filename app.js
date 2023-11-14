91982d86953c2db2069cf998f518eefdf5748f73
iyansr <ikhyan7@gmail.com>
2023-11-14 09:40:37 +0700

    port

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const PORT = process.env.PORT || 3034;
+
 const app = express();
 const path = require("path");
 const cors = require("cors");
@@ -14,7 +16,6 @@ app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: true, limit: "100mb", parameterLimit: 50000 }));
 app.use(bodyParser.json({ limit: "50mb", extended: true }));
 
-
 const appRoute = require("./app/routes/route-auth");
 const homeRoute = require("./app/routes/route-home");
 const userRoute = require("./app/routes/route-account");
@@ -25,11 +26,7 @@ console.log(path.join(__dirname, "uploads"));
 
 app.use("/public/uploads", express.static(path.join(__dirname, "uploads/")));
 
-app.use(
-  cors({
-    origin: ["https://portal.zisindosat.id", "http://localhost:3000"],
-  })
-);
+app.use(cors("*"));
 app.use("/auth", appRoute);
 app.use("/home", homeRoute);
 app.use("/user", userRoute);
@@ -40,6 +37,6 @@ app.get("/", (req, res) => {
   res.send("Selamat Datang Di Portal ZISWAF Indosat");
 });
 
-app.listen(3034, () => {
-  console.log("Server Berjalan di Port : 4800");
+app.listen(PORT, () => {
+  console.log(`Server Berjalan di Port : ${PORT}`);
 });
